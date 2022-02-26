b58d5f5fe8004a209f26bb0cadd4d5d55eaab14c
rafal-nowak <contact@rafal-nowak.com>
2022-02-26 09:48:39 +0100

    project initialization

---
+++ src/test/java/com/example/age_of_empires_2/AgeOfEmpires2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.age_of_empires_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeOfEmpires2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
