8068343e1ea31396c4372dc51495011652bc40f0
rafal-nowak <contact@rafal-nowak.com>
2022-02-26 11:18:00 +0100

    removed test endpoints

--- src/main/java/com/example/age_of_empires_2/api/CivilizationController.java
+++ src/main/java/com/example/age_of_empires_2/api/CivilizationController.java
@@ -5,7 +5,6 @@ import com.example.age_of_empires_2.external.api.response.Civilization;
 import com.example.age_of_empires_2.external.storage.CivilizationEntity;
 import com.example.age_of_empires_2.external.storage.CivilizationService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,25 +19,6 @@ public class CivilizationController {
 
     CivilizationApiService civilizationApiService;
     CivilizationService civilizationService;
-
-    @GetMapping("/civilizations")
-    List<Civilization> getAllCivilizations() throws IOException {
-        return civilizationApiService.getAllCivilizations();
-    }
-
-    @GetMapping("/civilizations-from-db")
-    List<CivilizationEntity> getAllCivilizationsFromDB() {
-        return civilizationService.findAll();
-    }
-
-    @GetMapping("/grab-civilizations")
-    List<Civilization> grabAllCivilizationToDB() throws IOException {
-        List<Civilization> civilizations = civilizationApiService.getAllCivilizations();
-
-        civilizationService.saveCivilizations(civilizations);
-
-        return civilizations;
-    }
 
     @GetMapping("/search")
     public List<CivilizationEntity> searchForCivilizations(
